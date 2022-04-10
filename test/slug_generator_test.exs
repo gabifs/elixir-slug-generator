274f9a1e6744e43338e1168a68059cdee3384aae
Gabriel Fernandes Silva <ga3riel.fernandess@gmail.com>
2022-04-10 14:50:55 -0300

    add tests for create

--- test/slug_generator_test.exs
+++ test/slug_generator_test.exs
@@ -2,7 +2,15 @@ defmodule SlugGeneratorTest do
   use ExUnit.Case
   doctest SlugGenerator
 
-  test "greets the world" do
-    assert SlugGenerator.hello() == :world
+  test "generates slug" do
+    s = "[\"Pattern\", \"Matching\", \"In\", \"Elixir\"] = [a, b, c, d]"
+
+    assert "pattern-matching-in-elixir-a-b-c-d" = SlugGenerator.create(s)
+  end
+
+  test "generates simple slug" do
+    s = "My slug generator & simple test"
+
+    assert "my-slug-generator-and-simple-test" = SlugGenerator.create(s)
   end
 end
